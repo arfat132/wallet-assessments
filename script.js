8d4a51d9fa1bbe713462814a8e16f927fdc8a14e
arfat132 <arfatbegum90@gmail.com>
2022-02-17 20:54:00 +0300

    edited

--- script.js
+++ script.js
@@ -13,59 +13,66 @@ let savingValue = document.getElementById('saving-value');
 let savingAmount = document.getElementById('saving-amount');
 let remainingAmount = document.getElementById('remaining-amount');
 
+//error 
 let errorIncome = document.getElementById('error-income');
 let errorFood = document.getElementById('error-food');
 let errorRent = document.getElementById('error-rent');
 let errorClothes = document.getElementById('error-clothes');
+let expensesError = document.getElementById('expenses-error');
 let suficientAmount = document.getElementById('suficient-amount');
 
 function addAmount(currentAmount, newAmount, secondAmount, thirdAmount) {
- return Number(currentAmount) + Number(newAmount) + Number(secondAmount) + Number(thirdAmount);
+  return Number(currentAmount) + Number(newAmount) + Number(secondAmount) + Number(thirdAmount);
+}
+
+function substractAmount(currentAmount, newAmount) {
+  return Number(currentAmount) - Number(newAmount);
+}
+
+// calculate button
+function calculate() {
+  const totalExpenses = addAmount(expensesBalance.innerText, foodvalue.value, rentvalue.value, clothesvalue.value);
+  expensesBalance.innerText = totalExpenses;
+
+  const balance = substractAmount(incomeBalance.value, totalExpenses);
+  currentBalance.innerText = balance;
+
+  //string error handle
+  if (isNaN(incomeBalance.value)) {
+    errorIncome.style.display = 'block';
   }
-  
-  function substractAmount(currentAmount, newAmount) {
-    return Number(currentAmount) - Number(newAmount);
+  else if (isNaN(foodvalue.value)) {
+    errorFood.style.display = 'block';
   }
-
-  // calculate button
-   function calculate(){
-      const totalExpenses = addAmount(expensesBalance.innerText, foodvalue.value, rentvalue.value, clothesvalue.value);
-      expensesBalance.innerText = totalExpenses;
-
-      const balance = substractAmount(incomeBalance.value, totalExpenses );
-      currentBalance.innerText = balance;
-    
-      //string error handle
-      if(isNaN(incomeBalance.value)){
-        errorIncome.style.display = 'block';       
-      }
-      else if(isNaN(foodvalue.value)){
-        errorFood.style.display = 'block';  
-      }
-      else if(isNaN(rentvalue.value)){
-        errorRent.style.display = 'block';  
-      }
-      else if(isNaN(clothesvalue.value)){
-        errorClothes.style.display = 'block';  
-      }
-
-     
-
+  else if (isNaN(rentvalue.value)) {
+    errorRent.style.display = 'block';
+  }
+  else if (isNaN(clothesvalue.value)) {
+    errorClothes.style.display = 'block';
   }
 
-  //save button
-  function saving(){
-      const savings = incomeBalance.value * savingValue.value;
-      const savingsAmount = savings / 100;
-      savingAmount.innerText = savingsAmount
-    
-    const remainingBalance = substractAmount(currentBalance.innerText, savingsAmount);
-    remainingAmount.innerText = remainingBalance;
-
-      
-    if (savingAmount.innerText > incomeBalance.value && savingAmount.innerText > currentBalance.innerText) {
-      alert('You have not suficient balance')
-      savingAmount.innerText = 0 ;
-      remainingAmount.innerText = 0 ; 
-    }
+  //negative number error handle
+  if (incomeBalance.value < 0) {
+    errorIncome.style.display = 'block';
   }
+  else if (foodvalue.value < 0) {
+    errorFood.style.display = 'block';
+  }
+  else if (rentvalue.value < 0) {
+    errorRent.style.display = 'block';
+  }
+  else if (clothesvalue.value < 0) {
+    errorClothes.style.display = 'block';
+  }
+}
+
+//save button
+function saving() {
+  const savings = incomeBalance.value * savingValue.value;
+  const savingsAmount = savings / 100;
+  savingAmount.innerText = savingsAmount
+
+  const remainingBalance = substractAmount(currentBalance.innerText, savingsAmount);
+  remainingAmount.innerText = remainingBalance;
+
+}
